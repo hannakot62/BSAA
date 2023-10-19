9c974d132cc3be8b7a583820a870bba748297b4d
HANNAKOT62 <2kotyagova6@gmail.com>
2023-10-19 13:11:46 +0300

    subjects slice

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import teachersReducer from "./slices/teachersSlice";
 import thunk from "redux-thunk";
+import subjectsReducer from "./slices/subjectsSlice";
 
 
 export const store = configureStore({
     reducer: {
-        teachers: teachersReducer
+        teachers: teachersReducer,
+        subjects: subjectsReducer
     },
     middleware: [thunk]
 })
---
+++ src/store/slices/subjectsSlice.js
@@ -0,0 +1,26 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+
+export const fetchSubjects = () => {
+    return function (dispatch) {
+        fetch("https://bgaa.by/test")
+            .then(response => response.json())
+            .then(json => {
+                dispatch(getTeachers(json.data))
+            })
+    }
+}
+
+export const subjectsSlice = createSlice({
+    name: "subjects",
+    initialState: {subjects: []},
+    reducers: {
+        getSubjects: (state, action) => {
+            state.subjects = action.payload
+        }
+    }
+})
+
+
+export const {getSubjects} = subjectsSlice.actions
+export default subjectsSlice.reducer
